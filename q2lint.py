ff8fb8e0a7e68b42b2b1018c0ec0181bfb1c530a
Jai Ram Rideout <jai.rideout@gmail.com>
2016-12-22 15:08:09 -0700

    ENH: validating Python license/copyright headers
    Pair-programmed with @thermokarst.

---
+++ q2lint.py
@@ -0,0 +1,46 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2016-2017, QIIME 2 development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
+import pathlib
+import sys
+
+
+def main():
+    license = pathlib.Path('LICENSE')
+    LICENSE = pathlib.Path(__file__).parent / 'LICENSE'
+
+    errors = []
+    if license.exists():
+        if license.read_text() != LICENSE.read_text():
+            errors.append('Invalid LICENSE file')
+    else:
+        errors.append('Missing LICENSE file')
+
+    for filepath in pathlib.Path('.').glob('**/*.py'):
+        with filepath.open('r') as filehandle:
+            header = ''.join(line for _, line in zip(range(7), filehandle))
+            if header != HEADER:
+                errors.append('Invalid header: %s' % filepath)
+
+    if errors:
+        sys.exit('\n'.join(errors))
+
+
+HEADER = """\
+# ----------------------------------------------------------------------------
+# Copyright (c) 2016-2017, QIIME 2 development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+"""
+
+
+if __name__ == '__main__':
+    main()
