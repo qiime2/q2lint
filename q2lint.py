5aa7b982d582eff94d5a9a3cccde2c9b81b8cc59
Max von Hippel <maxvonhippel1996@gmail.com>
2017-06-29 14:33:59 -0700

    ENH: Silence NPM Installation (#8)
    npm -i step of linter is now totally silent, unless it runs into an error.
    
    Fixes #6

--- q2lint.py
+++ q2lint.py
@@ -46,14 +46,18 @@ def main():
 
         for package in npm_packages:
             pkg_path = package.parent
-            cmd = 'cd %s && npm i && npm run build -- --bail && (git diff ' \
+            install = 'cd %s && npm i --silent --progress false' % pkg_path
+            cmd = subprocess.run(install, shell=True,
+                                 stdout=subprocess.DEVNULL)
+            if cmd.returncode != 0:
+                errors.append('npm install failed')
+            cmd = 'cd %s && npm run build -- --bail && (git diff ' \
                   '--quiet */bundle.js || bash -c "exit 42")' % pkg_path
-            res = subprocess.run(cmd, shell=True)
+            res = subprocess.run(cmd, shell=True, stdout=subprocess.DEVNULL)
             if res.returncode == 42:
                 errors.append('Bundle is out of sync for %s' % pkg_path)
             elif res.returncode != 0:
-                errors.append('npm build error on %s\n%s' %
-                              (pkg_path, res.stderr))
+                errors.append('npm build error on %s' % pkg_path)
 
     if errors:
         sys.exit('\n\n\033[91m%s\033[0m\n\n' % '\n'.join(errors))
