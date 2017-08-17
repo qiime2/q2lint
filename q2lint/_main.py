91f14cd7620de1941c7c38321c158df9c97e67aa
Evan Bolyen <ebolyen@gmail.com>
2017-08-17 11:24:09 -0700

    ENH: support variable copyright dates (#15)
    Fixes #13.

--- q2lint/_main.py
+++ q2lint/_main.py
@@ -6,9 +6,34 @@
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
 
+import itertools
 import argparse
+import datetime
 import pathlib
 import sys
+import re
+
+
+YEAR_PLACEHOLDER = "COPYRIGHT_YEARS"  # chosen to avoid re.escape replacement
+
+
+def check_license(license, reference, copyright_idx):
+    for idx, (line, ref) in enumerate(
+            itertools.zip_longest(license.splitlines(),
+                                  reference.splitlines())):
+        if line is None or ref is None:
+            return "does not match reference"
+
+        if idx == copyright_idx:
+            curr = datetime.datetime.now().year
+            # matches `2XXX-curr` or just `curr`
+            year_regex = '(?:2[0-9]{3}-%(curr)d|%(curr)d)' % {'curr': curr}
+            line_regex = re.escape(ref).replace(YEAR_PLACEHOLDER, year_regex)
+            if not re.fullmatch(line_regex, line):
+                return "incorrect copyright years or does not match reference"
+
+        elif line != ref:
+            return "does not match reference"
 
 
 def main():
@@ -23,8 +48,10 @@ def main():
 
     errors = []
     if license.exists():
-        if license.read_text() != LICENSE.read_text():
-            errors.append('Invalid LICENSE file')
+        reason = check_license(license.read_text(), LICENSE.read_text(),
+                               copyright_idx=2)
+        if reason:
+            errors.append("Invalid LICENSE file (%s)" % reason)
     else:
         errors.append('Missing LICENSE file')
 
@@ -35,8 +62,9 @@ def main():
             continue
         with filepath.open('r') as filehandle:
             header = ''.join(line for _, line in zip(range(7), filehandle))
-            if header != HEADER:
-                errors.append('Invalid header: %s' % filepath)
+            reason = check_license(header, HEADER, copyright_idx=1)
+            if reason:
+                errors.append('Invalid header: %s (%s)' % (filepath, reason))
             if filepath.name != 'setup.py':
                 continue
             text = filehandle.read()
@@ -75,7 +103,7 @@ def main():
 
 HEADER = """\
 # ----------------------------------------------------------------------------
-# Copyright (c) 2016-2017, QIIME 2 development team.
+# Copyright (c) COPYRIGHT_YEARS, QIIME 2 development team.
 #
 # Distributed under the terms of the Modified BSD License.
 #
