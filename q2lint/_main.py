qiime2/q2lint

commit d1f48291bd46c1b130ac84891d47b2acc13865aa

imp: header check for files with first line hash-pling (#26)

q2lint/_main.py
@@ -108,7 +108,11 @@ def validate_project(install_requires):
         if filepath.name in ('_version.py', 'versioneer.py'):
             continue
         with filepath.open('r') as filehandle:
-            header = ''.join(line for _, line in zip(range(7), filehandle))
+            header = list(line for _, line in zip(range(8), filehandle))
+            if re.match("^#!", header[0]):
+                header = ''.join(header[1:])
+            else:
+                header = ''.join(header[:7])
             reason = check_license(header, HEADER, copyright_idx=1)
             if reason:
                 errors.append('Invalid header: %s (%s)' % (filepath, reason))

commit f6927efb8d44494e34fa3d99075d009fb0e230ce

bug: explicit length checking (#27)
fixes bug introduced in #26

q2lint/_main.py
@@ -109,6 +109,11 @@ def validate_project(install_requires):
             continue
         with filepath.open('r') as filehandle:
             header = list(line for _, line in zip(range(8), filehandle))
+            if len(header) < 7:
+                errors.append('Invalid header: %s (%s)' % (
+                    filepath,
+                    "header missing/too short"))
+                continue
             if re.match("^#!", header[0]):
                 header = ''.join(header[1:])
             else: